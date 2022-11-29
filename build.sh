e28e789ee298a2479f9cbabf903ccdd40da61556
gfhe <heguangfu21@mails.ucas.ac.cn>
2022-11-29 10:02:50 +0000

    Improve dockerfile compose

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
-docker build . -f Dockerfile.0 -t hgfkeep/translation:base
-docker build . -t hgfkeep/translation:1129
+docker build . -f Dockerfile.0 -t hgfkeep/translation:torch-1.9.0-cu102
+docker build . -f Dockerfile.1 -t hgfkeep/translation:base
+docker build . -t hgfkeep/translation:$(date +%m%d)
+
+docker push hgfkeep/translation:torch-1.9.0-cu102
+docker push hgfkeep/translation:base
+docker push hgfkeep/translation:$(date +%m%d)
